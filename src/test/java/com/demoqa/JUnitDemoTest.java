8d003f6bbe7e1b95f1d0585f072d6b35b90372c0
novikoviam <novikoviam@gmail.com>
2022-10-03 00:46:35 +0300

    JUnitDemoTest added .gitignore file added

--- src/test/java/com/demoqa/JUnitDemoTest.java
+++ src/test/java/com/demoqa/JUnitDemoTest.java
@@ -1,12 +1,41 @@
 package com.demoqa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnitDemoTest {
 
+
+    @BeforeAll
+    static void setUp(){
+        System.out.println("### @BeforeAll      !");
+    }
+    @AfterAll
+    static void setUp(){
+        System.out.println("### @AfterAll       !");
+    }
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("### @BeforeEach !");
+    }
+
+    @AfterEach
+    void setUp(){
+        System.out.println("### @AfterEach  !");
+    }
+
+
     @Test
-    void simpleTest() {
+    void simpleTestFirst() {
+        System.out.println("### @Test simpleTestFirst() !");
+        Assertions.assertTrue(3>2);
+
+    }
+
+    @Test
+    void simpleTestSecond() {
+        System.out.println("### @Test simpleTestSecond() !");
         Assertions.assertTrue(3>2);
     }
+
 }
